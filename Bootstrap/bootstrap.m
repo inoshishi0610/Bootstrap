d8f99b4d56415538a1a999fce5df0d06b1b1c23b
inoshishi0610 <70304204+inoshishi0610@users.noreply.github.com>
2024-01-05 23:49:54 +0700

    Update bootstrap.m

--- Bootstrap/bootstrap.m
+++ Bootstrap/bootstrap.m
@@ -41,7 +41,7 @@ void rebuildSignature(NSString *directoryPath)
                 
                 if(ismacho) {
                     
-                    SYSLOG("重建 %@", enumURL.path);
+                    SYSLOG("rebuild %@", enumURL.path);
                     
                     machoCount++;
                     
@@ -59,7 +59,7 @@ void rebuildSignature(NSString *directoryPath)
         }
     }
     
-    SYSLOG("重建完成! machoCount=%d, libCount=%d", machoCount, libCount);
+    SYSLOG("rebuild finished! machoCount=%d, libCount=%d", machoCount, libCount);
 
 }
 
@@ -97,6 +97,14 @@ int buildPackageSources()
         ASSERT([[NSString stringWithFormat:@(ALT_SOURCES), getCFMajorVersion()] writeToFile:jbroot(@"/etc/apt/sources.list.d/sileo.sources") atomically:YES encoding:NSUTF8StringEncoding error:nil]);
     }
     
+    if(![fm fileExistsAtPath:jbroot(@"/var/mobile/Library/Application Support/xyz.willy.Zebra")])
+    {
+        NSDictionary* attr = @{NSFilePosixPermissions:@(0755), NSFileOwnerAccountID:@(501), NSFileGroupOwnerAccountID:@(501)};
+        ASSERT([fm createDirectoryAtPath:jbroot(@"/var/mobile/Library/Application Support/xyz.willy.Zebra") withIntermediateDirectories:YES attributes:attr error:nil]);
+    }
+    
+    ASSERT([[NSString stringWithFormat:@(ZEBRA_SOURCES), getCFMajorVersion()] writeToFile:jbroot(@"/var/mobile/Library/Application Support/xyz.willy.Zebra/sources.list") atomically:YES encoding:NSUTF8StringEncoding error:nil]);
+    
     return 0;
 }
 
@@ -123,27 +131,27 @@ int startBootstrapd()
     NSString* err=nil;
     int status = spawnRoot(jbroot(@"/basebin/bootstrapd"), @[@"daemon",@"-f"], &log, &err);
     if(status != 0) {
-        STRAPLOG("引导程序加载失败(%d):\n%@\nERR:%@", status, log, err);
+        STRAPLOG("bootstrap server load faild(%d):\n%@\nERR:%@", status, log, err);
         ABORT();
     }
 
-    STRAPLOG("引导程序加载成功");
+    STRAPLOG("bootstrap server load successful");
     
     sleep(1);
     
      status = spawnRoot(jbroot(@"/basebin/bootstrapd"), @[@"check"], &log, &err);
     if(status != 0) {
-        STRAPLOG("引导程序检查失败(%d):\n%@\nERR:%@", status, log, err);
+        STRAPLOG("bootstrap server check faild(%d):\n%@\nERR:%@", status, log, err);
         ABORT();
     }
-    STRAPLOG("引导程序检查成功");
+    STRAPLOG("bootstrap server check successful");
     
     return 0;
 }
 
 int InstallBootstrap(NSString* jbroot_path)
 {
-    STRAPLOG("安装引导程序...");
+    STRAPLOG("install bootstrap...");
     
     NSFileManager* fm = NSFileManager.defaultManager;
     
@@ -153,7 +161,7 @@ int InstallBootstrap(NSString* jbroot_path)
     NSString* bootstrapZstFile = [NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:
                                   [NSString stringWithFormat:@"strapfiles/bootstrap-%d.tar.zst", getCFMajorVersion()]];
     if(![fm fileExistsAtPath:bootstrapZstFile]) {
-        STRAPLOG("无法找到引导文件,可能此版本的应用不支持iOS%d", NSProcessInfo.processInfo.operatingSystemVersion.majorVersion);
+        STRAPLOG("can not find bootstrap file, maybe this version of the app is not for iOS%d", NSProcessInfo.processInfo.operatingSystemVersion.majorVersion);
         return -1;
     }
     
@@ -166,7 +174,7 @@ int InstallBootstrap(NSString* jbroot_path)
     NSString* tarPath = [NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"tar"];
     ASSERT(spawnRoot(tarPath, @[@"-xpkf", bootstrapTarFile, @"-C", jbroot_path], nil, nil) == 0);
     
-    STRAPLOG("重新构建引导二进制文件");
+    STRAPLOG("rebuild boostrap binaries");
     rebuildSignature(jbroot_path);
     
     NSString* jbroot_secondary = [NSString stringWithFormat:@"/var/mobile/Containers/Shared/AppGroup/.jbroot-%016llX", jbrand()];
@@ -196,18 +204,18 @@ int InstallBootstrap(NSString* jbroot_path)
                     withDestinationPath:jbroot_path error:nil]);
     
     
-    STRAPLOG("状态: 构建基础二进制文件");
+    STRAPLOG("Status: Building Base Binaries");
     ASSERT(rebuildBasebin() == 0);
     
-    STRAPLOG("状态: 启动引导程序");
+    STRAPLOG("Status: Starting Bootstrapd");
     ASSERT(startBootstrapd() == 0);
     
-    STRAPLOG("状态: 完成引导程序初始化");
+    STRAPLOG("Status: Finalizing Bootstrap");
     NSString* log=nil;
     NSString* err=nil;
     int status = spawnBootstrap((char*[]){"/bin/sh", "/prep_bootstrap.sh", NULL}, &log, &err);
     if(status != 0) {
-        STRAPLOG("失败(%d):%@\nERR:%@", status, log, err);
+        STRAPLOG("faild(%d):%@\nERR:%@", status, log, err);
         ABORT();
     }
 
@@ -220,16 +228,19 @@ int InstallBootstrap(NSString* jbroot_path)
     ASSERT(buildPackageSources() == 0);
     
     
-    STRAPLOG("状态: 正在安装插件包");
+    STRAPLOG("Status: Installing Packages");
     NSString* libkrw0_dummy = [NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"libkrw0-dummy.deb"];
     ASSERT(spawnBootstrap((char*[]){"/usr/bin/dpkg", "-i", rootfsPrefix(libkrw0_dummy).fileSystemRepresentation, NULL}, nil, nil) == 0);
     
     NSString* sileoDeb = [NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"sileo.deb"];
     ASSERT(spawnBootstrap((char*[]){"/usr/bin/dpkg", "-i", rootfsPrefix(sileoDeb).fileSystemRepresentation, NULL}, nil, nil) == 0);
     
+    NSString* zebraDeb = [NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"zebra.deb"];
+    ASSERT(spawnBootstrap((char*[]){"/usr/bin/dpkg", "-i", rootfsPrefix(zebraDeb).fileSystemRepresentation, NULL}, nil, nil) == 0);
+    
     ASSERT([[NSString stringWithFormat:@"%d",BOOTSTRAP_VERSION] writeToFile:jbroot(@"/.bootstrapped") atomically:YES encoding:NSUTF8StringEncoding error:nil]);
     
-    STRAPLOG("状态: 引导程序安装完成");
+    STRAPLOG("Status: Bootstrap Installed");
     
     
     return 0;
@@ -279,13 +290,13 @@ int ReRandomizeBootstrap()
     
     //jbroot() enabled
     
-    STRAPLOG("状态: 正在构建基础二进制文件");
+    STRAPLOG("Status: Building Base Binaries");
     ASSERT(rebuildBasebin() == 0);
     
-    STRAPLOG("状态: 正在启动引导程序");
+    STRAPLOG("Status: Starting Bootstrapd");
     ASSERT(startBootstrapd() == 0);
     
-    STRAPLOG("状态: 正在更新软链接");
+    STRAPLOG("Status: Updating Symlinks");
     ASSERT(spawnBootstrap((char*[]){"/bin/sh", "/usr/libexec/updatelinks.sh", NULL}, nil, nil) == 0);
     
     return 0;
@@ -295,23 +306,23 @@ int bootstrap()
 {
     ASSERT(getuid()==0);
     
-    STRAPLOG("引导中...");
+    STRAPLOG("bootstrap...");
     
     NSFileManager* fm = NSFileManager.defaultManager;
     
     NSString* jbroot_path = find_jbroot();
     
     if(!jbroot_path) {
-        STRAPLOG("设备未启动...");
+        STRAPLOG("device is not strapped...");
         
         jbroot_path = [NSString stringWithFormat:@"/var/containers/Bundle/Application/.jbroot-%016llX", jbrand_new()];
         
-        STRAPLOG("引导 @ %@", jbroot_path);
+        STRAPLOG("bootstrap @ %@", jbroot_path);
         
         ASSERT(InstallBootstrap(jbroot_path) == 0);
         
     } else if(![fm fileExistsAtPath:jbroot(@"/.bootstrapped")]) {
-        STRAPLOG("删除未完成的引导程序 %@", jbroot_path);
+        STRAPLOG("remove unfinished bootstrap %@", jbroot_path);
         
         uint64_t prev_jbrand = jbrand();
         
@@ -319,33 +330,31 @@ int bootstrap()
         
         NSString* jbroot_secondary = [NSString stringWithFormat:@"/var/mobile/Containers/Shared/AppGroup/.jbroot-%016llX", prev_jbrand];
         if([fm fileExistsAtPath:jbroot_secondary]) {
-            STRAPLOG("删除未完成的引导程序 %@", jbroot_secondary);
+            STRAPLOG("remove unfinished bootstrap %@", jbroot_secondary);
             ASSERT([fm removeItemAtPath:jbroot_secondary error:nil]);
         }
         
-        STRAPLOG("引导 @ %@", jbroot_path);
+        STRAPLOG("bootstrap @ %@", jbroot_path);
         
         ASSERT(InstallBootstrap(jbroot_path) == 0);
         
     } else {
-        STRAPLOG("设备已启动: %@", jbroot_path);
+        STRAPLOG("device is strapped: %@", jbroot_path);
         
-        STRAPLOG("状态: 重新随机化 jbroot");
+        STRAPLOG("Status: Rerandomize jbroot");
         
         ASSERT(ReRandomizeBootstrap() == 0);
     }
-
-    ASSERT(disableRootHideBlacklist()==0);
     
     ASSERT(disableRootHideBlacklist()==0);
     
-    STRAPLOG("状态: 重新构建应用程序");
+    STRAPLOG("Status: Rebuilding Apps");
     ASSERT(spawnBootstrap((char*[]){"/bin/sh", "/basebin/rebuildapps.sh", NULL}, nil, nil) == 0);
 
     NSDictionary* bootinfo = @{@"bootsession":getBootSession()};
     ASSERT([bootinfo writeToFile:jbroot(@"/basebin/.bootinfo.plist") atomically:YES]);
     
-    STRAPLOG("状态: 引导成功");
+    STRAPLOG("Status: Bootstrap Successful");
     
     return 0;
 }
@@ -361,7 +370,7 @@ int bootstrap()
 
 int unbootstrap()
 {
-    SYSLOG("取消引导...");
+    SYSLOG("unbootstrap...");
     
     NSFileManager* fm = NSFileManager.defaultManager;
     
@@ -373,7 +382,7 @@ int unbootstrap()
             continue;
         
         if(is_jbroot_name(item.UTF8String)) {
-            SYSLOG("删除 %@ @ %@", item, dirpath);
+            SYSLOG("remove %@ @ %@", item, dirpath);
             ASSERT([fm removeItemAtPath:[dirpath stringByAppendingPathComponent:item] error:nil]);
         }
     }
@@ -387,12 +396,12 @@ int unbootstrap()
             continue;
         
         if(is_jbroot_name(item.UTF8String)) {
-            SYSLOG("删除 %@ @ %@", item, dirpath);
+            SYSLOG("remove %@ @ %@", item, dirpath);
             ASSERT([fm removeItemAtPath:[dirpath stringByAppendingPathComponent:item] error:nil]);
         }
     }
 
-    SYSLOG("引导程序已卸载!");
+    SYSLOG("bootstrap uninstalled!");
     
     [LSApplicationWorkspace.defaultWorkspace _LSPrivateRebuildApplicationDatabasesForSystemApps:YES internal:YES user:YES];
     
